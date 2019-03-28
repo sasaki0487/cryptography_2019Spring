46dad162f568a482ed74d9d34ff9b44db8b52ee5
sasaki0487 <bobbytsai040@gmail.com>
2019-03-29 00:18:31 +0800

    data output & code update

--- hw3/aes-modes.cpp
+++ hw3/aes-modes.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <iomanip>
 
 #include "cryptopp/modes.h"
@@ -7,68 +8,29 @@
 #include "cryptopp/hex.h"
 
 using namespace std;
-
+using namespace CryptoPP;
 int main(int argc, char* argv[]) {
 
-    //Key and IV setup
-    //AES encryption uses a secret key of a variable length (128-bit, 196-bit or 256-
-    //bit). This key is secretly exchanged between two parties before communication
-    //begins. DEFAULT_KEYLENGTH= 16 bytes
-    CryptoPP::byte key[ CryptoPP::AES::DEFAULT_KEYLENGTH ], iv[ CryptoPP::AES::BLOCKSIZE ];
-    memset( key, 0x00, CryptoPP::AES::DEFAULT_KEYLENGTH );
-    memset( iv, 0x00, CryptoPP::AES::BLOCKSIZE );
-
-    stringstream ss;
+    fstream fout;
+    byte key[ AES::DEFAULT_KEYLENGTH ], iv[ AES::BLOCKSIZE ];
+    memset( key, 0x00, AES::DEFAULT_KEYLENGTH );
+    memset( iv, 0x00, AES::BLOCKSIZE );
 
     string keyin = "1234567890123456";
     string ivin = "0000000000000000";
     string dst;
 
-    for(int i = 0 ; i < CryptoPP::AES::DEFAULT_KEYLENGTH ; i++){
+    for(int i = 0 ; i < AES::DEFAULT_KEYLENGTH ; i++){
         key[i] = (int)keyin[i];
         iv[i] = (int)ivin[i];
     }
-    /*CryptoPP::StringSource ss1(keyin,true,new CryptoPP::HexEncoder(new CryptoPP::ArraySink(&key[0],16)));
-    CryptoPP::StringSource ss2(ivin,true,new CryptoPP::HexEncoder(new CryptoPP::ArraySink(&iv[0],16)));
-    key[0]= 0x31;
-    key[1]= 0x32;
-    key[2]= 0x33;
-    key[3]= 0x34;
-    key[4]= 0x35;
-    key[5]= 0x36;
-    key[6]= 0x37;
-    key[7]= 0x38;
-    key[8]= 0x39;
-    key[9]= 0x30;
-    key[10]= 0x31;
-    key[11]= 0x32;
-    key[12]= 0x33;
-    key[13]= 0x34;
-    key[14]= 0x35;
-    key[15]= 0x36;
 
-    iv[0]= 0x30;
-    iv[1]= 0x30;
-    iv[2]= 0x30;
-    iv[3]= 0x30;
-    iv[4]= 0x30;
-    iv[5]= 0x30;
-    iv[6]= 0x30;
-    iv[7]= 0x30;
-    iv[8]= 0x30;
-    iv[9]= 0x30;
-    iv[10]= 0x30;
-    iv[11]= 0x30;
-    iv[12]= 0x30;
-    iv[13]= 0x30;
-    iv[14]= 0x30;
-    iv[15]= 0x30;
-*/
     //
     // String and Sink setup
     //
-    //string plaintext = "AES is efficient in both software and hardware.";
-    string plaintext = "Hello World!";
+
+    string plaintext = "AES is efficient in both software and hardware.";
+    //string plaintext = "Hello World!";
     string ciphertext[4];
     string decryptedtext;
 
@@ -82,29 +44,33 @@ int main(int argc, char* argv[]) {
     CryptoPP::ECB_Mode_ExternalCipher::Encryption ecbEncryption2( aesEncryption);
 
     CryptoPP::StreamTransformationFilter stfEncryptor1(ecbEncryption1, new CryptoPP::StringSink( ciphertext[0] ) ,CryptoPP::StreamTransformationFilter::ZEROS_PADDING);
-    stfEncryptor1.Put( reinterpret_cast<const unsigned char*>( plaintext.c_str() ), plaintext.length() );
+    stfEncryptor1.Put( reinterpret_cast<const unsigned char*>( plaintext.c_str() ), plaintext.length() + 1);
     stfEncryptor1.MessageEnd();
 
-    CryptoPP::StreamTransformationFilter stfEncryptor2(ecbEncryption2, new CryptoPP::StringSink( ciphertext[1] ) ,CryptoPP::StreamTransformationFilter::DEFAULT_PADDING);
-    stfEncryptor2.Put( reinterpret_cast<const unsigned char*>( plaintext.c_str() ), plaintext.length() );
+    CryptoPP::StreamTransformationFilter stfEncryptor2(ecbEncryption2, new CryptoPP::StringSink( ciphertext[1] ) ,CryptoPP::StreamTransformationFilter::PKCS_PADDING);
+    stfEncryptor2.Put( reinterpret_cast<const unsigned char*>( plaintext.c_str() ), plaintext.length() + 1);
     stfEncryptor2.MessageEnd();
 
     CryptoPP::StreamTransformationFilter stfEncryptor3(cbcEncryption1, new CryptoPP::StringSink( ciphertext[2] ) ,CryptoPP::StreamTransformationFilter::ZEROS_PADDING);
-    stfEncryptor3.Put( reinterpret_cast<const unsigned char*>( plaintext.c_str() ), plaintext.length() );
+    stfEncryptor3.Put( reinterpret_cast<const unsigned char*>( plaintext.c_str() ), plaintext.length() + 1);
     stfEncryptor3.MessageEnd();
 
-    CryptoPP::StreamTransformationFilter stfEncryptor4(cbcEncryption2, new CryptoPP::StringSink( ciphertext[3] ) ,CryptoPP::StreamTransformationFilter::DEFAULT_PADDING);
-    stfEncryptor4.Put( reinterpret_cast<const unsigned char*>( plaintext.c_str() ), plaintext.length() );
+    CryptoPP::StreamTransformationFilter stfEncryptor4(cbcEncryption2, new CryptoPP::StringSink( ciphertext[3] ) ,CryptoPP::StreamTransformationFilter::PKCS_PADDING);
+    stfEncryptor4.Put( reinterpret_cast<const unsigned char*>( plaintext.c_str() ), plaintext.length() + 1);
     stfEncryptor4.MessageEnd();
 
+    fout.open("Out.txt",ios::out);
 
-
-        for( int i = 0; i < 4 ; i++ ) {
-            for(int j = 0 ; j < ciphertext[i].size(); j++){
-                cout << hex << (0xFF & static_cast<CryptoPP::byte>(ciphertext[i][j]));
-            }
-            cout << endl;
+    for( int i = 0; i < 4 ; i++ ) {
+        for(int j = 0 ; j < ciphertext[i].size(); j++){
+            cout << hex << (0xFF & static_cast<byte>(ciphertext[i][j]));
+            fout << hex << (0xFF & static_cast<byte>(ciphertext[i][j]));
         }
+        cout << endl << endl;
+        fout << endl << endl;
+    }
+
+    fout.close();
 
     //
     // Decrypt
